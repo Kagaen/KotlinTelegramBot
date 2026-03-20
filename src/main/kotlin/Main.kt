a97025fb4a845e81813e38ad795bda40fd36a300
stasenko <alipsid@mail.ru>
2026-03-20 13:13:53 +0300

    KTB-02 fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,8 @@ import java.io.File
 
 fun main() {
 
-    val words: File = File("src\\main\\kotlin\\words.txt")
+    val words: File = File("words.txt")
     val stringList = words.readLines()
-    repeat(stringList.size) { str: Int ->
-        println(stringList[str])
-    }
+    stringList.forEach { println(it) }
 
 }

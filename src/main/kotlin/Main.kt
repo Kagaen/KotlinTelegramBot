686b055131ef08b6f959ffa0d0802538c357192c
stasenko <alipsid@mail.ru>
2026-03-21 11:02:41 +0300

    KTB-04 report

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,25 @@ import java.io.File
 
 fun main() {
 
-    val words: File = File("words.txt")
+    val wordsFile: File = File("words.txt")
+    val dictionary = mutableListOf<Word>()
     try {
-        val stringList = words.readLines()
-        stringList.forEach { println(it) }
+        val stringList = wordsFile.readLines()
+        stringList.forEach {
+            val split = it.split("|")
+            val line: Word = Word(split[0], split[1], split[2].toIntOrNull() ?: 0)
+            dictionary.add(line)
+        }
+        dictionary.forEach { line -> println(line) }
     } catch (e: Exception) {
         println("Ошибка: ${e.message}")
     }
 
 }
+
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int,
+)
+
